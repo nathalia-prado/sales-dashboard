89c1d6b9f546674e18cb318f841e500e844f6ed0
Nathalia Prado <nathalia_898@hotmail.com>
2023-09-04 00:12:36 +1200

    Relace URL for jpg images to make loading faster

--- client/components/RateCard.tsx
+++ client/components/RateCard.tsx
@@ -7,13 +7,13 @@ function RateCard({cardInfo} : {cardInfo: RateData}) {
       <div className="rate-card-div">
         <ul className="rate-card-ul">
           <li className="rate-card-li">
-            <img className="rate-card-img" src={cardInfo.employeePhoto} alt={`Employee ${cardInfo.employeeName}`} />
+            <img className="rate-card-img" src={`assets/${cardInfo.employeePhoto}`} alt={`Employee ${cardInfo.employeeName}`} />
           </li>
           <li className="rate-card-li rate-card-li-name">
             <p className="rate-card-p">{cardInfo.employeeName}</p>
           </li>
           <li className="rate-card-li">
-            <Rating name="simple-controlled" value={cardInfo.averageRate} readOnly={true} />
+            <Rating name="simple-controlled" value={cardInfo.averageRate} readOnly={true} precision={0.5} />
           </li>
         </ul>
       </div>
